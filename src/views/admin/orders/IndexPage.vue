cf3fff3ac1a570676e4f8853e35cce89858a1b50
minhoo0730 <zxcvc12339@gmail.com>
2023-04-26 00:04:42 +0900

    feat: 관리자페이지 라우터 세팅
    관리자페이지 라우터 세팅 및 메뉴 생성
    POSTCSS 세팅

---
+++ src/views/admin/orders/IndexPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>주문관리페이지</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/admin/sales/IndexPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>SalesTitle</h2>
+  </div>
+  <div>SalesContent</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
