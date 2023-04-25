22492de06001b19a703371b2d4389daa6090274e
minhoo0730 <zxcvc12339@gmail.com>
2023-04-26 00:20:27 +0900

    feat: 관리자페이지 라우터 세팅2
    라우터 경로 및 파일 재배치

--- src/router/index.js
+++ src/router/index.js
@@ -2,36 +2,36 @@ import { createRouter, createWebHistory } from 'vue-router';
 const routes = [
   {
     path: '/',
-    component: () => import('@/views/order/LayoutPage.vue'),
+    component: () => import('@/order/LayoutPage.vue'),
   },
   {
     path: '/login',
-    component: () => import('@/views/auth/LoginPage.vue'),
+    component: () => import('@/auth/LoginPage.vue'),
   },
   {
     path: '/admin',
     redirect: '/admin/sales',
-    component: () => import('@/views/admin/LayoutPage.vue'),
+    component: () => import('@/admin/LayoutPage.vue'),
     children: [
       {
         path: 'sales',
         name: 'sales',
-        component: () => import('@/views/admin/sales/IndexPage.vue'),
+        component: () => import('@/admin/views/sales/IndexPage.vue'),
       },
       {
         path: 'menus',
         name: 'menus',
-        component: () => import('@/views/admin/menus/IndexPage.vue'),
+        component: () => import('@/admin/views/menus/IndexPage.vue'),
       },
       {
         path: 'users',
         name: 'users',
-        component: () => import('@/views/admin/users/IndexPage.vue'),
+        component: () => import('@/admin/views/users/IndexPage.vue'),
       },
       {
         path: 'orders',
         name: 'orders',
-        component: () => import('@/views/admin/orders/IndexPage.vue'),
+        component: () => import('@/admin/views/orders/IndexPage.vue'),
       },
     ],
   },
