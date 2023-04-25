1f6b1e193c29cb314bd0fee09761b2d8ef42290f
minhoo0730 <zxcvc12339@gmail.com>
2023-04-26 00:04:24 +0900

    feat: 관리자페이지 라우터 세팅
    관리자페이지 라우터 세팅 및 메뉴 생성
    POSTCSS 세팅

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,34 @@ const routes = [
   },
   {
     path: '/login',
+    component: () => import('@/views/auth/LoginPage.vue'),
+  },
+  {
+    path: '/admin',
+    redirect: '/admin/sales',
     component: () => import('@/views/admin/LayoutPage.vue'),
+    children: [
+      {
+        path: 'sales',
+        name: 'sales',
+        component: () => import('@/views/admin/sales/IndexPage.vue'),
+      },
+      {
+        path: 'menus',
+        name: 'menus',
+        component: () => import('@/views/admin/menus/IndexPage.vue'),
+      },
+      {
+        path: 'users',
+        name: 'users',
+        component: () => import('@/views/admin/users/IndexPage.vue'),
+      },
+      {
+        path: 'orders',
+        name: 'orders',
+        component: () => import('@/views/admin/orders/IndexPage.vue'),
+      },
+    ],
   },
 ];
 
--- src/views/admin/LayoutPage.vue
+++ src/views/admin/LayoutPage.vue
@@ -1,9 +1,30 @@
 <template>
-  <div class="text-3xl font-bold underline">로그인페이지</div>
+  <header id="header">
+    <div class="header-area">
+      <div class="logo"><a href="/admin">LOGO</a></div>
+      <nav class="nav">
+        <ul>
+          <li><router-link to="/admin/sales">매출관리</router-link></li>
+          <li><router-link to="/admin/menus">메뉴관리</router-link></li>
+          <li><router-link to="/admin/users">사용자관리</router-link></li>
+          <li><router-link to="/admin/orders">주문관리</router-link></li>
+        </ul>
+      </nav>
+      <div class="user-area">
+        <button>검색</button>
+        <button>알림</button>
+        <button>사용자페이지 버튼</button>
+      </div>
+    </div>
+  </header>
+  <div id="content">
+    <router-view></router-view>
+  </div>
+  <footer id="footer">Footer</footer>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style lang="postcss" scoped></style>
