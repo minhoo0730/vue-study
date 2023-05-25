6d2b6180529155f39ae203fda726852fec5dc3e2
minhoo0730 <zxcvc12339@gmail.com>
2023-05-25 21:59:22 +0900

    feat: 관리자 - 메뉴관리 - 메뉴추가 모달팝업 생성

---
+++ frontend/src/admin/components/Modal.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    class="modal"
+    :class="modal.modalName"
+    aria-labelledby="modal-title"
+    role="dialog"
+    aria-modal="true"
+  >
+    <!--
+    Background backdrop, show/hide based on modal state.
+
+    Entering: "ease-out duration-300"
+      From: "opacity-0"
+      To: "opacity-100"
+    Leaving: "ease-in duration-200"
+      From: "opacity-100"
+      To: "opacity-0"
+  -->
+    <div class="modal-bg"></div>
+
+    <div class="modal-component">
+      <div class="modal-container">
+        <!--
+        Modal panel, show/hide based on modal state.
+
+        Entering: "ease-out duration-300"
+          From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          To: "opacity-100 translate-y-0 sm:scale-100"
+        Leaving: "ease-in duration-200"
+          From: "opacity-100 translate-y-0 sm:scale-100"
+          To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+      -->
+        <div class="modal-area">
+          <div class="modal-header">
+            <h3 class="" id="modal-title">{{ modal.title }}</h3>
+            <i class="ri-close-line"></i>
+          </div>
+          <!-- <div class="bg-white p-20">
+            <div class="sm:flex sm:items-start"></div>
+          </div> -->
+          <!-- <div class="modal-content">
+            <p>
+              Are you sure you want to deactivate your account? All of your data
+              will be permanently removed. This action cannot be undone.
+            </p>
+          </div> -->
+          <slot name="modalContent"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modal: Object,
+  },
+  setup() {},
+};
+</script>
+
+<style lang="postcss" scoped>
+.modal-header {
+  @apply flex justify-between items-center;
+  > i {
+    font-size: 2.6rem;
+    cursor: pointer;
+  }
+}
+</style>
---
+++ frontend/src/admin/views/menus/AddMenuView.vue
@@ -0,0 +1,45 @@
+<template>
+  <modal :modal="{ title, modalName }">
+    <template v-slot:modalContent>
+      <div class="modal-content">
+        <div class="form-box">
+          <label for="menu-name">메뉴이름</label>
+          <input
+            type="text"
+            name="menu-name"
+            id="menu-name"
+            class="block w-full rounded-md border-0 px-16 py-16 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:leading-6"
+          />
+        </div>
+        <div>메뉴설명</div>
+        <div>가격</div>
+        <div>카테고리</div>
+        <div>이미지</div>
+      </div>
+      <div class="modal-footer">
+        <button>취소</button>
+        <button>저장</button>
+      </div>
+    </template>
+  </modal>
+</template>
+
+<script>
+import Modal from '@/admin/components/Modal.vue';
+import { ref } from 'vue';
+export default {
+  components: {
+    Modal,
+  },
+  setup() {
+    const title = ref('메뉴추가');
+    const modalName = ref('AddMenu');
+    return {
+      title,
+      modalName,
+    };
+  },
+};
+</script>
+
+<style></style>
